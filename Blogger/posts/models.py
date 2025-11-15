ffa5707c2ea2b2e77f9d64921fba78817f0f875a
lama <leema442@gmail.com>
2025-11-15 20:41:25 +0300

    add lab

---
+++ Blogger/posts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    """
+    title : char field (max 2048)
+    content : text field.
+    is_published : boolean field, default is True.
+    published_at : datetime field, default is now.
+    """
+
+    title=models.CharField(max_length=300)
+    content=models.TextField()
+    is_published=models.BooleanField(default=True)
+    published_at=models.DateTimeField(default=timezone.now)
+
---
+++ Blogger/posts/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import path
+from . import views
+
+
+
+app_name = "posts"
+
+
+urlpatterns = [
+    path('', views.home_view, name='home_view'),
+    path('add/', views.add_post_view, name='add_post_view'),
+
+
+ ]
